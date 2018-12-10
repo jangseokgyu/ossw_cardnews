c448e914f9817cd5005dbc3240474fa578c42834
Unknown <jsk053@hanyang.ac.kr>
2018-12-10 18:48:15 +0900

    수정

--- uploading.php
+++ uploading.php
@@ -2,56 +2,46 @@
 <html>
 
 <body>
+  <?php
+  $allowedExts = array("gif", "jpeg", "jpg", "png");
 
-<form action="uploading.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+  if (isset($_FILES)) {
+      $file = $_FILES["file"];
+      $error = $file["error"];
+      $name = $file["name"];
+      $type = $file["type"];
+      $size = $file["size"];
+      $tmp_name = $file["tmp_name"];
 
-<?php
-$target_dir = "C:\Users\administer\gitkraken\cardnews\ossw_cardnews\assets\images_cardnews";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-?>
+      if ( $error > 0 ) {
+          echo "Error: " . $error . "<br>";
+      }
+      else {
+          $temp = explode(".", $name);
+          $extension = end($temp);
+
+          if ( ($size/1024/1024) < 2.) || in_array($extension, $allowedExts) ) {
+              //echo "Upload: " . $name . "<br>";
+              //echo "Type: " . $type . "<br>";
+              //echo "Size: " . ($size / 1024 / 1024) . " Mb<br>";
+              //echo "Stored in: " . $tmp_name;
+              if (file_exists("upload/" . $name)) {
+                  echo $name . " already exists. ";
+              }
+              else {
+                  move_uploaded_file($tmp_name, "upload/" . $name);
+                  echo "Stored in: " . "upload/" . $name;
+              }
+          }
+          else {
+              echo ($size/1024/1024) . " Mbyte is bigger than 2 Mb ";
+              echo $extension . "format file is not allowed to upload ! ";
+          }
+      }
+  }
+  else {
+      echo "File is not selected";
+  }
+  ?> 
 </body>
 </html>
