4d0381acb84fbc9e438dcd537451e485705959ed
Unknown <jsk053@hanyang.ac.kr>
2018-12-12 12:48:18 +0900

    amend

--- uploading.php
+++ uploading.php
@@ -10,7 +10,7 @@ $uploaddir = 'C:\Bitnami\wampstack-7.1.23-0\apache2\htdocs\ossw_cardnews\assets\
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
+    echo "파일의 업로드가 성공하였습니다..\n";
 } else {
     print "파일 업로드 공격의 가능성이 있습니다!\n";
 }
