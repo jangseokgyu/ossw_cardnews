20012c5087b0c1e5311042f4084b38d74d7137ee
Unknown <jsk053@hanyang.ac.kr>
2019-01-12 15:16:24 +0900

    web game demo ver

---
+++ assets/js/test.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",drawScreen,false);
+
+function drawScreen()
+{
+  var theCanvas = document.getElementById("GameCanvas");
+  var Context = theCanvas.getContext("2d");
+
+  Context.fillStyle = "#000000";
+  Context.fillRect(0,0,800,600);
+}
+
+drawScreen();
